cd9c6a2a576df197e4c8c04774446892ff51e8c5
TovenaarAndreas <andreas.veeckman@tovenaargames.be>
2025-04-09 12:05:07 +0200

    Added root flow tag

---
+++ Source/Flow/Private/FlowTags.cpp
@@ -0,0 +1,7 @@
+// Copyright TovenaarGames, All Rights Reserved
+
+
+#include "FlowTags.h"
+
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_Flow, "Flow");
---
+++ Source/Flow/Public/FlowTags.h
@@ -0,0 +1,8 @@
+// Copyright TovenaarGames, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
+
+FLOW_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_Flow)
