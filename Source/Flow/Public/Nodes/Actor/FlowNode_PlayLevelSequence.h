474461a46e5e404a909274d93b791c39cb20d35b
TovenaarAndreas <andreas.veeckman@tovenaargames.be>
2026-09-22 11:02:47 +0200

    Adjusting displayname on PlayLevelSequence node

--- Source/Flow/Public/Nodes/Actor/FlowNode_PlayLevelSequence.h
+++ Source/Flow/Public/Nodes/Actor/FlowNode_PlayLevelSequence.h
@@ -21,7 +21,7 @@ DECLARE_MULTICAST_DELEGATE(FFlowNodeLevelSequenceEvent);
  * - Out (always, even if Sequence is invalid)
  * - Completed
  */
-UCLASS(NotBlueprintable, meta = (DisplayName = "INVALID Play Level Sequence"), Abstract)
+UCLASS(NotBlueprintable, meta = (DisplayName = "Play Level Sequence"))
 class FLOW_API UFlowNode_PlayLevelSequence : public UFlowNode
 {
 	GENERATED_UCLASS_BODY()
